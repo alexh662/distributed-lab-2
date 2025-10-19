81b0d6f8553f698a4b8b86d1d1901210e792d4ec
alex <ss24495@bristol.ac.uk>
2025-10-19 17:46:36 +0100

    completed secret strings

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,46 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"log"
+	"net/rpc"
+	"os"
+
+	//	"bufio"
+	//	"os"
+	//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
+
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+	filePath := flag.String("file", "../wordlist", "Path to file containing words")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	file, err := os.Open(*filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		word := scanner.Text()
+
+		if word == "" {
+			continue
+		}
+
+		request := stubs.Request{Message: word}
+		response := new(stubs.Response)
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println(response.Message)
+	}
 }
